c6720b74783b5994bd692b3064dc77fee2c2184b
JW <kiw970923@naver.com>
2024-05-09 15:22:46 +0900

    [fix] 블로그 게시물 상세보기 페이지에서 메모 동작하지 않던 현상 수정
    #3

--- src/main/resources/static/js/blog/postList.js
+++ src/main/resources/static/js/blog/postList.js
@@ -2,47 +2,5 @@
 		// 카테고리 UI 이벤트 등록
 		addCategoryUIEvent();
 		addSortableEvent();
-		// 메모 삭제 버튼
-		$(".delete-memo-btn").on("click", function(){
-			let memoId = $(this).data("id");
-			$.ajax({
-				url: "/blog/memo/remove"
-				, type: "DELETE"
-				, data: {"memoId":memoId}
-				, success:function(data){
-					if(data.result == "success"){
-						location.reload();
-					}else{
-						alert("메모 삭제 실패");
-					}
-				}
-				, error:function(){
-					alert("메모 삭제 오류");
-				}
-			})
-		});
-		
-		// 메모 작성
-		$("#memoForm").on("submit", function(e){
-			e.preventDefault();
-			let content = $("#memoInput").val();
-			if(content == ""){
-				alert("메묘 내용을 입력하세요.");
-			}
-			$.ajax({
-				url: "/blog/memo/upload"
-				, type: "POST"
-				, data: {"content":content}
-				, success:function(data){
-					if(data.result == "success"){
-						location.reload();
-					}else{
-						alert("메모 작성 실패");
-					}
-				}
-				, error:function(){
-					alert("메모 작성 에러");
-				}
-			});
-		});
+
 	});
